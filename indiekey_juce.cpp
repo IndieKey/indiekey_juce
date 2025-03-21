836c95a015b9006aec970f1361ae31649c1102c3
Ruurd Adema <ruurd@owllab.nl>
2025-03-21 13:23:11 +0100

    Add JUCE module spec (ACT-120)

---
+++ indiekey_juce.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ruurd Adema on 21/03/2025.
+// Copyright (c) 2025 Sound on Digital. All rights reserved.
+//
+
+#include "src/Activation.cpp"
+#include "src/ActivationClient.cpp"
+#include "src/ActivationsDatabase.cpp"
+#include "src/Crypto.cpp"
+#include "src/RestClient.cpp"
---
+++ indiekey_juce.h
@@ -0,0 +1,15 @@
+/*
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:               indiekey_juce
+    vendor:           IndieKey
+    version:          0.1.0
+    name:             IndieKey client SDK for JUCE
+    description:      Classes to unlock your software by communicating with the IndieKey server
+    website:          https://indiekey.io
+    license:          AGPLv3/Commercial
+    searchpaths:      include
+    dependencies:     juce_core
+
+    END_JUCE_MODULE_DECLARATION
+*/
